4c53da766a78fc7f1f0bc0cc63f23367fb0bce62
Shredder1612 <anshumanshukla1612@gmail.com>
2026-05-22 17:09:15 +0530

    add all auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,16 +1,53 @@
 import { Router } from "express";
-import { registerUser, login, logoutUser } from "../controllers/auth.controller.js";
+import {
+  registerUser,
+  login,
+  logoutUser,
+  verifyEmail,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  getCurrentUser,
+  changeCurrentPassword,
+  resendEmailVerification,
+} from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegisterValidator, userLoginValidator } from "../validators/index.js";
-import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {
+  userRegisterValidator,
+  userLoginValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator,
+} from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// unsecured routes
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refersh-token").post(refreshAccessToken);
+router
+  .route("/forgot-password")
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest);
+router
+  .route("/reset-password/:resetToken")
+  .post(userResetForgotPasswordValidator(), validate, resetForgotPassword);
 
 //secure routes
 router.route("/logout").post(verifyJWT, logoutUser);
-
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router
+  .route("/change-password")
+  .post(
+    verifyJWT,
+    userChangeCurrentPasswordValidator(),
+    validate,
+    changeCurrentPassword,
+  );
+router
+  .route("/reset-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
